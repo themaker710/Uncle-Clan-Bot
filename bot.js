c3ce14d2470110916a13b3d8dc8101bf57e01032
themaker710 <mcm05nate@gmail.com>
2020-06-16 17:29:37 +1000

    experimental

--- bot.js
+++ bot.js
@@ -1,27 +1,30 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-	client.user.setActivity("Testing"); 
+'use strict';
 
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+const config = require('./config.json');
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+	console.log('Initialized');
+	//client.user.setStatus('idle') // Options: invisible, available/online, dnd, idle
+	client.user.setActivity('ur mum', { type: 'WATCHING' }) // type options: WATCHING, PLAYING, STREAMING
+  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+  .catch(console.error);
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+    // It will listen for messages that will start with `-`
     if (message.substring(0, 1) == '-') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -30,7 +33,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // -ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
@@ -39,3 +42,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(config.token);
