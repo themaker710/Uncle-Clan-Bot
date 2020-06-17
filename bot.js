28e14cb8ab6e54ea35fae3021e9f0bceafaa9541
themaker710 <mcm05nate@gmail.com>
2020-06-17 19:49:54 +1000

    General Changes

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@
  */
 
 // Import the discord.js module
-const Discord = require('discord.js');
-const config = require('./config.json');
+const { Client, MessageEmbed } = require('discord.js');
+const { prefix, token } = require('./config.json');
 // Create an instance of a Discord client
-const client = new Discord.Client();
+const client = new Client();
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -16,35 +16,68 @@ const client = new Discord.Client();
  */
 client.on('ready', () => {
 	console.log('Initialized');
-	//client.user.setStatus('idle') // Options: invisible, available/online, dnd, idle
-	client.user.setActivity('ur mum', { type: 'WATCHING' }) // type options: WATCHING, PLAYING, STREAMING
-  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+	//client.user.setStatus('dnd') // Options: invisible, online, dnd, idle
+	//.then(status => console.log(`Status set to ${presence.status}`))
+ // .catch(console.error);
+	client.user.setActivity('with kate', { type: 'PLAYING' }) // type options: WATCHING, PLAYING, STREAMING
+  .then(presence => console.log(`Activity set to ${presence.activities[0].type} ${presence.activities[0].name}`))
   .catch(console.error);
 });
 
 client.on('message', message => {
   // If the message is "ping"
-  if (message.content.charAt(0) == '-') {
-    // Send "pong" to the same channel
-   
-  var msg = message.content.substring(1);
-console.log(`Command Entered: ${msg}`);
-   
-switch(msg) {
-            // -ping
-            case 'ping':
-                 message.channel.send('pong');
-            break;
-			default:
-				
-				// var str1 = msg.concat(' is not a valid command');
-				
-				message.channel.send(`The command '${msg}' is not valid. Please contact the developer if this is a unexpected result`);
-			break;
-};
+ // console.log(config.prefix.length);
+ 
+ 
+if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  }
-});
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+ // if (message.content.charAt(0) == '-') {
+    // Send "pong" to the same channel
+    if (command === 'args') {
+		if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		};
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	} else if (command === 'anal') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to anal them!');
+		}
+		const taggedUser = message.mentions.users.first();
+		
+		if (message.author.id == taggedUser.id) {
+			message.channel.send(`${message.author} analed themselves.... Hurrah?`);
+		}
+		else {
+
+		message.channel.send(`${message.author} analed ${taggedUser}! Hurrah!`);
+		}
+	} else if (command === 'abs') {
+		
+		message.channel.send('With Pleasure:', {files: ["https://i.ibb.co/Y72hgjD/image0.jpg"]});
+	} else if (command === 'kate') {
+	
+		message.channel.send('With Pleasure:', {files: ["https://i.ibb.co/VYZbBjs/katie.png"]});
+	} else if (command === 'avatar') {
+    const user = message.mentions.users.first() || message.author;
+	console.log(user.avatarURL);
+    const avatarEmbed = new MessageEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.avatarURL);
+    message.channel.send(avatarEmbed);
+	message.channel.send(user.avatarURL);
+};
+	
+ // var msg = message.content.substring(1);
+console.log(`Command Entered: ${command} by ${message.author}`);
+   
+
+
+  });
+
 
 /* client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
@@ -70,4 +103,4 @@ switch(msg) {
 }); */
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
-client.login(config.token);
+client.login(token);
