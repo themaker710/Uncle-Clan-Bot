f1959ea84fb3d8a53641c7d1a807aad8a62f2575
themaker710 <mcm05nate@gmail.com>
2020-06-27 10:06:02 +1000

    logging, external batch execution

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 'use strict';
 
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
 // Import the discord.js module
 const { Client, MessageEmbed } = require('discord.js');
 const { prefix, token } = require('./config.json');
 // Create an instance of a Discord client
 const client = new Client();
-
+const shell = require('shelljs')
+let fs = require('fs')
+let logs = fs.createWriteStream('log.txt') //will creat log.txt if does not exist
 /**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * The ready event is vital, it means that only after this will your bot start reacting to information
  * received from Discord
  */
 client.on('ready', () => {
@@ -22,6 +20,7 @@ client.on('ready', () => {
 	client.user.setActivity('with kate', { type: 'PLAYING' }) // type options: WATCHING, PLAYING, STREAMING
   .then(presence => console.log(`Activity set to ${presence.activities[0].type} ${presence.activities[0].name}`))
   .catch(console.error);
+logs.write('Log of commands entered: {command} by {user} in {guild/server name}\n')
 });
 
 client.on('message', message => {
@@ -35,10 +34,10 @@ if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const command = args.shift().toLowerCase();
 
 if (command.charAt(0) == "") return;	
-	
-	
-console.log(`Command Entered: ${command} by ${message.author.username}`);
 
+	
+console.log(`Command Entered: ${command} by ${message.author.username} in ${message.guild}`);
+logs.write(`${command} by ${message.author.username} in ${message.guild}\n`); // Writes to log.txt with \n = newline
 
 switch(command){
 		case 'test':
@@ -46,9 +45,13 @@ switch(command){
 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		};
 		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-		message.channel.send(`Argument 2: ${args[1]}`);
+		message.channel.send(`Argument 2: ${args[1]}`); // returns second arg (from 0)
 		break;
 		
+		case 'ping':
+		
+		message.channel.send('pong');
+		break;
 		
 		case 'do':
 		const taggedUser = message.mentions.users.first();
@@ -105,10 +108,14 @@ switch(command){
 		case 'kate':
 		message.channel.send('With Pleasure:', {files: ["https://i.ibb.co/VYZbBjs/katie.png"]});
 		break;
+		case 'server':
+		 shell.exec('start cmd.exe /c "server.bat"', {async:true}); //opens temp cmd window executing the specified command. Use /k instead for persistent window
+		 //shell.exit(1); //this terminates entire bot
+		 message.channel.send('The Minecraft server is starting up, please wait up to 3 minutes before running this command again.'); // add commmand cooldown
+		break;
 		default:
 		message.channel.send(`${message.author}, the command '${command}' is not a valid command! Please contact the developer if this is an unexpected occurrence.`)
 };
-
 
 
 
