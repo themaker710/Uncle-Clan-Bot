b92d1b744b52348f1b18bb8140f7a7c5d7376c9a
themaker710 <mcm05nate@gmail.com>
2020-07-20 17:49:02 +1000

    .gitignore
    Code blocks and .gitignore for log file

--- commands/help.js
+++ commands/help.js
@@ -34,9 +34,9 @@ module.exports = {
 			return message.reply('that\'s not a valid command!');
 		}
 
-		data.push(`**Name:** ${command.name}`);
+		data.push(`**Name:** \`${command.name}\``);
 
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.aliases) data.push(`**Aliases:** \`${command.aliases.join(', ')}\``);
 		if (command.description) data.push(`**Description:** ${command.description}`);
 		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
